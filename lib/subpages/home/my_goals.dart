746ece1a4230491ec54a9505bb825a8827fe2e24
jackOfnoTraits <119508192+jackOfnoTraits@users.noreply.github.com>
2024-03-22 09:53:13 +0530

    hello

--- lib/subpages/home/my_goals.dart
+++ lib/subpages/home/my_goals.dart
@@ -1,22 +1,93 @@
 import 'package:flutter/material.dart';
 
-class MyGoalsPage extends StatefulWidget {
+class MyGoalsPage extends StatelessWidget {
   const MyGoalsPage({super.key});
 
   @override
-  State<MyGoalsPage> createState() => _MyGoalsPageState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: TodoListScreen(),
+    );
+  }
 }
 
-class _MyGoalsPageState extends State<MyGoalsPage> {
+class TodoListScreen extends StatefulWidget {
+  @override
+  _TodoListScreenState createState() => _TodoListScreenState();
+}
+
+class _TodoListScreenState extends State<TodoListScreen> {
+  List<String> _goals = [];
+
+  void _addGoal(String goal) {
+    setState(() {
+      _goals.add(goal);
+    });
+  }
+
+  void _removeGoal(int index) {
+    setState(() {
+      _goals.removeAt(index);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Goals'),
+        title: Text("Goals"),
       ),
-      body: Container(
-        color: Colors.pink,
+      body: ListView.builder(
+        itemCount: _goals.length,
+        itemBuilder: (BuildContext context, int index) {
+          return ListTile(
+            title: Text(_goals[index]),
+            trailing: IconButton(
+              icon: Icon(Icons.delete),
+              onPressed: () => _removeGoal(index),
+            ),
+          );
+        },
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _showAddGoalDialog(context);
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+
+  void _showAddGoalDialog(BuildContext context) {
+    String newGoal = '';
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Add Goal'),
+          content: TextField(
+            onChanged: (value) {
+              newGoal = value;
+            },
+            decoration: InputDecoration(hintText: 'Enter your goal'),
+          ),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                _addGoal(newGoal);
+                Navigator.of(context).pop();
+              },
+              child: Text('Add'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Cancel'),
+            ),
+          ],
+        );
+      },
     );
   }
 }
